d0f31205f509b03a32868a583c728e9ffc9e199e
Seiba <a.seibatu9@gmail.com>
2021-08-17 20:12:55 +0100

    adding new info

---
+++ new_file.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;
+    cout<<"Hi Alima\n";
+    cout<<"Hi Alima and I am 17 years of age\n";
+    return 0;
+
+}
