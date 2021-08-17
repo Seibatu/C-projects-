b62ac851e6b8bdd0ec1c9571e5bf05b2cdf707b4
Seiba <a.seibatu9@gmail.com>
2021-08-17 20:15:59 +0100

    adding new info of amina

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(){
 
     cout<<"Hello World"<<endl;
-    cout<<"Hi Alima"<<"\n";
+    cout<<"Hi Alima\n";
+    cout<<"Hi Alima and I am 17 years of age\n";
     return 0;
 
 }
